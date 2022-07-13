968bd5dcd3e1e49f82668ba1c3e3ec2868e0157d
Remco Tolsma <869674+remcotolsma@users.noreply.github.com>
2022-07-13 16:13:48 +0200

    Clarify iDEAL issuers method.

--- src/Gateway.php
+++ src/Gateway.php
@@ -64,7 +64,7 @@ class Gateway extends Core_Gateway {
 
 		$ideal_issuer_field = new SelectField( 'ideal-issuer' );
 		$ideal_issuer_field->set_options_callback( function() {
-			return $this->get_issuers();
+			return $this->get_ideal_issuers();
 		} );
 
 		$ideal_payment_method->add_field( $ideal_issuer_field );
@@ -74,11 +74,11 @@ class Gateway extends Core_Gateway {
 	}
 
 	/**
-	 * Get issuers.
+	 * Get iDEAL issuers.
 	 *
 	 * @return array<int, array<string, array<string>>>
 	 */
-	private function get_issuers() {
+	private function get_ideal_issuers() {
 		return array(
 			array(
 				'options' => array(
